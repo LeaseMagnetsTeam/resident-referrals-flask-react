1c3f4df7f8c0963d7c93e6cae940c9f00e1597d4
Ace Figueroa <ace.g.fig@gmail.com>
2021-01-15 17:17:13 -0800

    init user/apartments model

--- backend/app/models.py
+++ backend/app/models.py
@@ -14,6 +14,16 @@ FOLLOWUPS = [
 ]
 
 
+def initialize_db(app, tearDown=False):
+    app.app_context().push()
+    db.init_app(app)
+
+    if tearDown:
+        db.drop_all()
+        db.create_all()
+
+
+
 class ConstantsModel(db.Model):
     __tablename__ = "constants"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -63,3 +73,31 @@ class Lead(TimestampModel, db.Model):
 
     def __repr__(self):
         return f"<Lead {self.id}>"
+
+# Apartments/user models --remove before pushing
+
+class Apartment(db.Model):
+    __tablename__ = "apartments"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(200), default="")
+
+    def __repr__(self):
+        return f"<Apartment {self.id}>"
+
+
+# https://stackoverflow.com/questions/18807322/sqlalchemy-foreign-key-relationship-attributes
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(200), default="")
+    phoneNumber = db.Column(db.String(50), default="")
+    email = db.Column(db.String(200), default="")
+    role = db.Column(db.String(50), default="") 
+    apartment = db.Column(db.Integer, db.ForeignKey(Apartment.id))
+
+    def __repr__(self):
+        return f"<User {self.id}>"
+
+
+
+
